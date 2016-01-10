beb82e8363aeea0898a7e2ae5c3a5d604ca0109d
Sarah Chau <schau.ta@gmail.com>
2016-01-09 23:28:24 -0500

    ExData_Plotting1
    Exploratory Data Analysis - Assignment 1

---
+++ plot3.R
@@ -0,0 +1,14 @@
+setwd("C:/Users/Sarah/Dropbox/R Tutorials/Household Power Data/")
+
+# Load required data for the two days
+data <- read.table("household_power_consumption.txt", skip = 66637, nrows = 2880, sep = ";", na.strings = "?", col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time), format = "%Y-%m-%d %H:%M:%S")
+
+#plot
+png("plot3.png", width = 480, height = 480)
+plot(data$DateTime, data$Sub_metering_1, type = "l", ylab="Energy sub metering", xlab = "")
+lines(data$DateTime, data$Sub_metering_2, col = "red")
+lines(data$DateTime, data$Sub_metering_3, col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), col = c("black", "red", "blue"))
+dev.off()
